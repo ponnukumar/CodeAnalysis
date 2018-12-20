e1c9502b7f3112b513fc6b3c6ba400b61de5510a
ponnukumar <45874903+ponnukumar@users.noreply.github.com>
2018-12-20 17:28:43 +0530

    Update HomeController.cs

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
 
 namespace WebApplication1.Controllers
 {
@@ -16,6 +17,7 @@ namespace WebApplication1.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            DataSet ds = new DataSet();
 
             return View();
         }
